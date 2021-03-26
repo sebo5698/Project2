d8594b38a7b03f98f72cb0409f7a7e7c4292a260
sebo5698 <sebo5698@colorado.edu>
2021-03-26 15:04:21 -0700

    Starting the function

---
+++ readPurchases.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Product.h"
+#include "Customer.h"
+#include <fstream>
+
+using namespace std;
+
+
+int readPurchases(string Filename,Customer arrcust[],int customstored,int custarrsize,int maxcol)
+{
+
+
+}
