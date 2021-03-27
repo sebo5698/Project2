465625bd4b8e5c114c6c8908cee823f4506881eb
sebo5698 <sebo5698@colorado.edu>
2021-03-27 01:55:40 -0700

    Starting the function

---
+++ getPurchasedQuantity.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Product.h"
+#include "Customer.h"
+#include <fstream>
+
+using namespace std;
+
+int getPurchasedQuantity (string customename,string prodname,Customer CustomrsArr[],Product prodsArr[],int custarrsi,int productArrsiz)
+{
+    
+}
