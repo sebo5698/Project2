858aed17b7915b55c7ae73774e6b6060b620d8d9
sebo5698 <sebo5698@colorado.edu>
2021-03-28 14:29:14 -0700

    some advances in comments

--- addCustomerDriver.cpp
+++ addCustomerDriver.cpp
@@ -11,6 +11,20 @@
 
 using namespace std;
 
+// This function adds a customer with the values of all purchases at 0 to the customers array
+/*
+    Parameters:
+        * name of the customer to be added(string)
+        * array of Customer objects
+        * number of products(int)
+        * number of customers currently stored(int)
+        * capacity of the customers array(int)
+    Return:
+        * if numCustomersStored >= customersArrSize, return -2
+        * if a customer with the same name already exists, return -1
+        * if name is empty, return -1
+        * add the customer object to the array and return the new total number of customers in the array
+*/
 int addCustomer(string customerName,Customer customerarr[],int numprods,int custmstored,int custarrsize)
 {
     bool namfound=false;
@@ -44,4 +58,26 @@ int addCustomer(string customerName,Customer customerarr[],int numprods,int cust
         return custmstored+1;
      }
     }
+}
+// test case
+int main()
+{        
+    Customer customers[10];
+    int customersArrSize = 10;
+    int numProducts  = 5;
+
+    customers[0].setCustomerName("Knuth");
+    customers[1].setCustomerName("Richie");
+
+    for(int i=0; i<5; i++) 
+    {
+        customers[0].setPurchasesAt(i, i);
+        customers[1].setPurchasesAt(i, 5-i);
+    }
+
+    int numCustomersStored = 2;
+
+    int val = addCustomer("Ninja", customers, numProducts, numCustomersStored, customersArrSize);
+
+    cout << val << endl;
 }
