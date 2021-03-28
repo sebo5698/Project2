416a0d714875fe887959a9f263a04e4c3b79a77f
sebo5698 <sebo5698@colorado.edu>
2021-03-28 14:27:11 -0700

    some advances in comments

--- customerDriver.cpp
+++ customerDriver.cpp
@@ -11,7 +11,50 @@ using namespace std;
 //this is my testing ground
 int main()
 {
-int productspur[50];
-Customer Customer1("Jhonn",productspur,50);
+	
+// Checking Paramterized Constructor
+int purchases[] = {3,4,5};
+Customer c = Customer("Punith", purchases, 3);
+cout << c.getCustomerName() << endl;
+cout << "getPurchasesAt(0) = " << c.getPurchasesAt(0) << endl;
+cout << "getPurchasesAt(1) = "<< c.getPurchasesAt(1) << endl;
+cout << "getPurchasesAt(2) = "<< c.getPurchasesAt(2) << endl;
+cout << "getPurchasesAt(9) = "<< c.getPurchasesAt(9) << endl;
+cout << "getPurchasesAt(29) = "<< c.getPurchasesAt(29) << endl;
+cout << "getPurchasesAt(49) = "<< c.getPurchasesAt(49) << endl;
+cout << "getPurchasesAt(-9) = "<< c.getPurchasesAt(-9) << endl;
+cout << "getPurchasesAt(-29) = "<< c.getPurchasesAt(-29) << endl;
+cout << "getPurchasesAt(52) = "<< c.getPurchasesAt(52) << endl;
+
+	
+// Checking Setter and Getter for Customer Name
+c.setCustomerName("John");
+cout << c.getCustomerName() << endl;
+
+	
+// Checking Getter for Size
+cout << c.getSize() << endl;
+
+// Checking Getter for Total Purchase Count
+c.setCustomerName("Jimmy");
+for(int i=0; i<c.getSize(); i+=15){
+c.setPurchasesAt(i,i);
+}
+cout << c.getCustomerName() << "'s Total Purchase Count is: " << c.getTotalPurchaseCount() << endl;
+
+
+// Checking Getter for Total Purchase Count
+c.setCustomerName("Jimmy");
+for(int i=0; i<c.getSize(); i+=10){
+    c.setPurchasesAt(i,i);
+}
+cout << c.getCustomerName() << "'s Total Purchase Count is: " << c.getTotalPurchaseCount() << endl;
+	
+// Checking Getter for Number of Unique Purchases
+c.setCustomerName("Jimmy");
+for(int i=1; i<c.getSize(); i+=10){
+    c.setPurchasesAt(i,i);
+}
+cout << c.getCustomerName() << "'s Number of Unique Purchases is: " << c.getNumUniquePurchases() << endl;
 
 }
