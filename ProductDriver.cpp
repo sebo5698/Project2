7acd50b3239ce19c5a488719cc9b78e79119b860
sebo5698 <sebo5698@colorado.edu>
2021-03-28 13:21:46 -0700

    Added two files were left

---
+++ ProductDriver.cpp
@@ -0,0 +1,33 @@
+// CS1300 Spring 2021
+// Author: Sergio Borrego
+// Recitation: 119 – Christen Malloy
+// Homework 7 - Product Driver
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Product.h"
+//this is my test for the product implementation
+int main()
+{
+    //i create a product
+    string name="Ryzen pcgmer";
+    double pricepc=300.00;
+    string typepr="Electronics";
+    Product Pcgaming(name,pricepc,typepr);
+
+    //check if the products are in order with the values assigned
+    cout<<Pcgaming.getName()<<" This is the name set"<<endl;
+    cout<<Pcgaming.getPrice()<<" This is the price set"<<endl;
+    cout<<Pcgaming.getCategory()<<" This is the category"<<endl;
+
+
+    //Check if the set functions work correctly
+    Pcgaming.setName("New pc gaming ryzen");
+    Pcgaming.setPrice(499.99);
+    Pcgaming.setCategory("Gaming Stuff");
+
+    //and if the new values assigned are get
+    cout<<Pcgaming.getName()<<" This is the new name set"<<endl;
+    cout<<Pcgaming.getPrice()<<" This is the new price set"<<endl;
+    cout<<Pcgaming.getCategory()<<" This is the new category"<<endl;
+}
---
+++ customerDriver.cpp
@@ -0,0 +1,17 @@
+// CS1300 Spring 2021
+// Author: Sergio Borrego
+// Recitation: 119 – Christen Malloy
+// Homework 7 - Product Driver
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Customer.h"
+
+using namespace std;
+//this is my testing ground
+int main()
+{
+int productspur[50];
+Customer Customer1("Jhonn",productspur,50);
+
+}
